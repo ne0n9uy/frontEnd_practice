375478ab174ef758fb731821b7b36f97f6ce7765
ne0n9uy <2leebins@naver.com>
2023-02-03 18:35:00 +0900

    fin without animation

--- 01_accordian/accordian.js
+++ 01_accordian/accordian.js
@@ -1,9 +1,20 @@
-const panelFaqContainer=document.querySelectorAll('.panel-faq-container');
-console.log(panelFaqContainer); //nodeList 객체로 가져옴
+window.onload=()=>{
 
-//반복문 순회하며 해당 FAQ 제목 클릭 시 콜백 처리
-for(let i=0; i<panelFaqContainer.length; i++){
+    //panel-faq-container
+    const panelFaqContainer=document.querySelectorAll('.panel-faq-container');
+    console.log(panelFaqContainer); //nodeList 객체로 가져옴
+    
+    //panel-faq-answer
+    let panelFaqAnswer=document.querySelectorAll('.panel-faq-answer');
+    // console.log(panelFaqAnswer[i]);
+    
+    //반복문 순회하며 해당 FAQ 제목 클릭 시 콜백 처리
+    for(let i=0; i<panelFaqContainer.length; i++){
     panelFaqContainer[i].addEventListener('click',function(){//클릭 시 이벤트
-        console.log('clicked',i);
+        console.log('clicked',i+1);
+        
+        //FAQ 제목 클릭-> 본문 보이게끔 -> active 클래스 추가
+        panelFaqAnswer[i].classList.add('active');
     });
+    }
 }
